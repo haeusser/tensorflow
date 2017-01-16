42e985236ba98839746c49daaeeb4f9964c592ae
Philip Häusser <haeusser@cs.tum.edu>
2017-01-16 17:43:31 +0100

    semisup package building. still need to test the examples.

--- tensorflow/contrib/semisup/__init__.py
+++ tensorflow/contrib/semisup/__init__.py
@@ -8,7 +8,17 @@ from __future__ import division
 from __future__ import print_function
 
 # pylint: disable=unused-import,line-too-long,g-importing-member,wildcard-import
-from tensorflow.contrib.semisup.python.semisup import *
+from tensorflow.contrib.semisup.python.semisup import semisup
+from tensorflow.contrib.semisup.python.semisup import train
+from tensorflow.contrib.semisup.python.semisup import eval
+
+from tensorflow.contrib.semisup.python.semisup import mnist_train_eval
+
+from tensorflow.contrib.semisup.python.semisup import mnist_tools
+from tensorflow.contrib.semisup.python.semisup import stl10_tools
+from tensorflow.contrib.semisup.python.semisup import svhn_tools
+from tensorflow.contrib.semisup.python.semisup import synth_tools
+
 from tensorflow.python.util.all_util import make_all
 # pylint: enable=unused-import,line-too-long,g-importing-member,wildcard-import
 
