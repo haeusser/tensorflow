86ae6f460b4b039d10ccf47d8a51977a7a0768a4
Philip Häusser <haeusser@cs.tum.edu>
2017-01-19 17:58:47 +0100

    MNIST example working.

--- tensorflow/contrib/semisup/__init__.py
+++ tensorflow/contrib/semisup/__init__.py
@@ -8,17 +8,20 @@ from __future__ import division
 from __future__ import print_function
 
 # pylint: disable=unused-import,line-too-long,g-importing-member,wildcard-import
-from tensorflow.contrib.semisup.python.semisup import semisup
-from tensorflow.contrib.semisup.python.semisup import train
-from tensorflow.contrib.semisup.python.semisup import eval
 
-from tensorflow.contrib.semisup.python.semisup import mnist_train_eval
-
+from tensorflow.contrib.semisup.python.semisup.semisup import *
 from tensorflow.contrib.semisup.python.semisup import mnist_tools
-from tensorflow.contrib.semisup.python.semisup import stl10_tools
-from tensorflow.contrib.semisup.python.semisup import svhn_tools
-from tensorflow.contrib.semisup.python.semisup import synth_tools
 
+"""
+mnist_tools = tensorflow.contrib.semisup.python.semisup.mnist_tools
+stl10_tools = tensorflow.contrib.semisup.python.semisup.stl10_tools
+svhn_tools = tensorflow.contrib.semisup.python.semisup.svhn_tools
+synth_tools = tensorflow.contrib.semisup.python.semisup.synth_tools
+
+eval = tensorflow.contrib.semisup.python.semisup.eval
+train = tensorflow.contrib.semisup.python.semisup.train
+mnist_train_eval = tensorflow.contrib.semisup.python.semisup.mnist_train_eval
+"""
 from tensorflow.python.util.all_util import make_all
 # pylint: enable=unused-import,line-too-long,g-importing-member,wildcard-import
 
