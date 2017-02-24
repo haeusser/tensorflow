eb83e7e87d68835564486399518ef232d62797d8
Philip Häusser <haeusser@cs.tum.edu>
2017-02-24 14:26:27 +0100

    semisup python module changes

--- tensorflow/contrib/semisup/__init__.py
+++ tensorflow/contrib/semisup/__init__.py
@@ -15,7 +15,7 @@ from tensorflow.contrib.semisup.python.semisup import svhn_tools
 from tensorflow.contrib.semisup.python.semisup import stl10_tools
 from tensorflow.contrib.semisup.python.semisup import synth_tools
 from tensorflow.contrib.semisup.python.semisup import architectures
-#from tensorflow.contrib.semisup.python.semisup import data_dirs
+from tensorflow.contrib.semisup.python.semisup import data_dirs
 
 from tensorflow.python.util.all_util import make_all
 # pylint: enable=unused-import,line-too-long,g-importing-member,wildcard-import
