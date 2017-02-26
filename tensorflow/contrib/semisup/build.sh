d5c309c5503374379c7cf7c4ec15177656a0a443
Philip Häusser <haeusser@cs.tum.edu>
2017-02-26 21:03:08 +0100

    Updated build rules for semisup

--- tensorflow/contrib/semisup/build.sh
+++ tensorflow/contrib/semisup/build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 # to be run from tensorflow root directory
-bazel build -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-msse4.2 --config=cuda -k --jobs=8 //tensorflow/contrib/semisup:train
-bazel build -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-msse4.2 --config=cuda -k --jobs=8 //tensorflow/contrib/semisup:eval
-#bazel build -c opt --config=cuda --jobs=8 //tensorflow/contrib/semisup:eval
+bazel build --config=opt --config=cuda --verbose_failures //tensorflow/contrib/semisup:train
+bazel build --config=opt --config=cuda --verbose_failures //tensorflow/contrib/semisup:eval
